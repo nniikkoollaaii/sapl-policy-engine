1d9d2bd749cec1a12b082a0d65eee3e1d9170935
Benedikt Heinrich <benedikt.heinrich@web.de>
2019-09-10 22:58:37 +0200

    added geth on linux for easier use of startChain

--- sapl-ethereum/src/test/java/io/sapl/interpreter/pip/EthereumModuleTest.java
+++ sapl-ethereum/src/test/java/io/sapl/interpreter/pip/EthereumModuleTest.java
@@ -1,5 +1,20 @@
 package io.sapl.interpreter.pip;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class EthereumModuleTest {
+
+    private EthereumPolicyInformationPoint ethPip;
+
+    @Before
+    public void init() {
+
+    }
+
+    @Test
+    public void convertToTypeShouldReturnCorrectValueWithAddressType() {
+
+    }
 
 }
