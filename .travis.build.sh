45d03d716f0a47f7ce8e6d129454846fa6f28c36
Felix Siegrist <felix.siegrist@inventage.com>
2019-11-15 20:03:52 +0100

    Deploy 2.0.0.RC1

--- .travis.build.sh
+++ .travis.build.sh
@@ -6,16 +6,20 @@ if [ -z "${TRAVIS}" ]; then
     TRAVIS_PULL_REQUEST="false"
 fi
 
-if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-        echo "Building master"
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=http://sonar.ftk.de:9000  -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/* --batch-mode
-        mvn deploy -DskipTests=true -Dmaven.javadoc.skip=true --batch-mode
-    else
-        echo "Building pull request"
-        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --batch-mode
-    fi
-else
-    echo "Building branch ${TRAVIS_BRANCH}"
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --batch-mode
-fi
+#if [ "${TRAVIS_BRANCH}" == "master" ]; then
+#    if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+#        echo "Building master"
+#        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dsonar.host.url=http://sonar.ftk.de:9000  -Dsonar.login=${SONAR_TOKEN} -Dsonar.exclusions=**/xtext-gen/**/*,**/xtend-gen/**/*,**/emf-gen/**/* --batch-mode
+#        mvn deploy -DskipTests=true -Dmaven.javadoc.skip=true --batch-mode
+#    else
+#        echo "Building pull request"
+#        mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --batch-mode
+#    fi
+#else
+#    echo "Building branch ${TRAVIS_BRANCH}"
+#    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent verify --batch-mode
+#fi
+
+echo "Building branch master-thesis-siegrist-felix"
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install --batch-mode
+mvn deploy -DskipTests=true -Dmaven.javadoc.skip=true --batch-mode
