f0e8f2c599202b9a5e604354868b1a84d6d0bd09
Nikolai Seip <nikolai-seip@web.de>
2020-12-20 13:48:14 +0100

    feat(PolicyImplCustomUnitTest): call super.matches instead of duplicate code

--- sapl-lang/src/main/java/io/sapl/grammar/sapl/impl/PolicyImplCustomUnitTest.java
+++ sapl-lang/src/main/java/io/sapl/grammar/sapl/impl/PolicyImplCustomUnitTest.java
@@ -17,30 +17,12 @@ public class PolicyImplCustomUnitTest extends PolicyImplCustom {
 	
 	@Override
 	public Mono<Val> matches(EvaluationContext ctx) {
-		log.trace("| | |-- PolicyElement test match '{}'", getSaplName());
-		
 		//additionally check for Policy Id to only match the unit tested policy
 		if (!getSaplName().equals(this.policyIdUnderUnitTest)) {
+			log.trace("Overriding match result with false due to wrong unit test policy id");
 			return Mono.just(Val.FALSE);
+		} else {
+			return super.matches(ctx);
 		}
-		
-		
-		final Expression targetExpression = getTargetExpression();
-		if (targetExpression == null) {
-			log.trace("| | | |-- MATCH (no target expression, matches all)");
-			log.trace("| | |");
-			return Mono.just(Val.TRUE);
-		}
-		return targetExpression.evaluate(ctx, Val.UNDEFINED).next().defaultIfEmpty(Val.FALSE).flatMap(result -> {
-			if (result.isError() || !result.isBoolean()) {
-				log.trace("| | | |-- ERROR in target expression did not evaluate to boolean. Was: {}", result);
-				log.trace("| | |");
-				return Val.errorMono(CONDITION_NOT_BOOLEAN, result);
-			}
-			log.trace("| | | |-- {}", result.get().asBoolean() ? "MATCH" : "NO MATCH");
-			log.trace("| | |");
-			return Mono.just(result);
-		});
 	}
-
 }
